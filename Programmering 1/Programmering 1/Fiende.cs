4c1dcbda5eb4c68b85bdc3d2b90d8b9a2d4b1185
Tim Tuvestam <timtuvestam@hotmail.se>
2019-02-26 23:07:40 +0100

    Projekt added

---
+++ Programmering 1/Programmering 1/Fiende.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Programmering_1
+{
+    class Fiende
+    {
+        Vector2 position;
+        Texture2D texture;
+        Vector2 center;
+        float angle = 0;
+
+        public Fiende(Texture2D texture, Vector2 position)
+        {
+            this.texture = texture;
+            this.center = position;
+        }
+
+        public void Update()
+        {
+            angle -= .1f;
+            position.X = center.X + (float)Math.Sin(angle) * 30;
+            position.Y = center.Y + (float)Math.Cos(angle) * 30;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, new Rectangle(position.ToPoint(), new Point(15,15)), Color.White);
+        }
+    }
+}
---
+++ Programmering 1/Programmering 1/Game1.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace Programmering_1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Texture2D tex;
+        MouseState oldState;
+        List<Fiende> fiendeLista = new List<Fiende>();
+
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            CreateTexture();
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if(Mouse.GetState().LeftButton == ButtonState.Pressed && oldState.LeftButton == ButtonState.Released)
+            {
+                // Lägg till objekt
+                fiendeLista.Add(new Fiende(tex, Mouse.GetState().Position.ToVector2()));
+            }
+            oldState = Mouse.GetState();
+
+            // Uppdatera listan
+            foreach (var item in fiendeLista)
+            {
+                item.Update();
+            }
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            foreach (var item in fiendeLista)
+            {
+                item.Draw(spriteBatch);
+            }
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+
+        void CreateTexture()
+        {
+            Color[] data = new Color[1];
+            data[0] = Color.White;
+            tex = new Texture2D(GraphicsDevice, 1, 1);
+            tex.SetData(data);
+        }
+    }
+}
